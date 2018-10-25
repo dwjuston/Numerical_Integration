f126508d69db8dfd57304b605110331eca2370a4
dwjuston <43318129+dwjuston@users.noreply.github.com>
2018-10-25 12:53:41 -0400

    Update Trapozoidal Rule.h

--- Trapozoidal Rule.h
+++ Trapozoidal Rule.h
@@ -2,25 +2,6 @@
 #define TRAPOZOIDAL_H
 
 #include <functional>
-/*
-
-Input:
-a = left endpoint of the integration interval
-b = right endpoint of the integration interval
-n = number of partition intervals
-f_int(x) = routine evaluating f(x)
-
-Output:
-I_trap = Trapezoidal Rule approximation
-
-h = (b-a)/n; 
-I_trap = f_int(a)/2 + f_int(b)/2
-for i = 1 : n
-	I_trap += f_int( a + ih  )
-end
-I_trap = h * I_trap
-
-*/
 
 double Trapezoidal(double a, double b, size_t n, std::function<double(double)> f_int)
 {
